fa53faeeb07f1a81ef2b4367de9d2be101686ad1
Miguel De leon <miguel4575@Me.com>
2022-06-18 02:32:00 -0400

    Add files via upload
    Updated files

--- JOINTECH/App.py
+++ JOINTECH/App.py
@@ -68,101 +68,7 @@ def insert_data(name,email,timestamp,skills):
     cursor.execute(insert_sql, rec_values)
     connection.commit()
 
-def webscraper():
-    # this was used for the person contacting me who had these details for their system
-    headers = {
-    "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36"}
 
-    
-    no_of_pages = 5
-
-    indeed_posts=[]
-
-    for page in range(no_of_pages):
-        url = 'https://www.indeed.com/jobs?q=' + skill + \
-        '&l=' + place + '&sort=date' +'&start='+ str(page * 10)
-        response = requests.get(url, headers=headers)
-        html = response.text
-
-    # Scrapping the Web
-        soup = BeautifulSoup(html, 'lxml')
-        
-
-    # Outer Most Entry Point of HTML:
-        outer_most_point=soup.find('div',attrs={'id': 'mosaic-provider-jobcards'})
-    # "UL" lists where the data are stored:
-        for i in outer_most_point.find('ul'):
-
-            # Job Title:
-            job_title=i.find('h2',{'class':'jobTitle jobTitle-color-purple jobTitle-newJob'})
-            # print(job_title)
-            if job_title != None:
-                jobs=job_title.find('a').text
-
-
-    
-
-
-            # Company Name:
-            if i.find('span',{'class':'companyName'}) != None:
-                company=i.find('span',{'class':'companyName'}).text
-            # Links:
-            if i.find('a') != None:
-                links=i.find('a',{'class':'jcs-JobTitle'})['href']
-            # Salary if available:
-            if i.find('div',{'class':'attribute_snippet'}) != None:
-                salary=i.find('div',{'class':'attribute_snippet'}).text
-            else:
-                salary='No Salary'
-
-        # Job Post Date:
-
-            if i.find('span', attrs={'class': 'date'}) != None:
-                post_date = i.find('span', attrs={'class': 'date'}).text
-
-            # Put everything together in a list of lists for the default dictionary
-            indeed_posts.append([company,jobs,links,salary, post_date])
-            print(indeed_posts)
-        # put together in list
-
-        # (create a dictionary with keys and a list of values from above "indeed_posts")
-
-    indeed_dict_list=defaultdict(list)
-
-    # Fields for our DF
-
-    indeed_spec=['Company','job','link','Salary','Job_Posted_Date']
-
-
-    import re # regular expressions
-    import datetime # format date/time
-
-    # Let's deal with fixing posting dates using Regular Expressions
-
-    indeed_jobs_df = pd.DataFrame(indeed_posts,columns=indeed_spec)
-
-
-    dates_converted=[]
-
-    for i in indeed_jobs_df['Job_Posted_Date']:
-
-        if re.findall(r'[0-9]',i):
-        # if the string has digits convert each entry to single string: ['3','0']->'30'
-            b=''.join(re.findall(r'[0-9]',i))
-        # convert string int to int and subtract from today's date and format
-            g=(datetime.datetime.today()-datetime.timedelta(int(b))).strftime('%m-%d-%Y')
-
-            dates_converted.append(g)
-        # if i.find('today') or i.find('just posted'):
-        # p=(datetime.datetime.today().strftime('%m-%d-%Y'))
-        # v.append(p)
-        else: # this will contain strings like: 'just posted' or 'today' etc before convert
-            dates_converted.append(datetime.datetime.today().strftime('%m-%d-%Y'))
-        dates_converted
-    indeed_jobs_df['Job_Posted_Date']= dates_converted
-
-    df = pd.DataFrame(indeed_posts,columns=indeed_spec)
-    st.dataframe(df)
 st.set_page_config(
    page_title="JoinTech",
    page_icon='./Logo/Join-Tech-Logo.png',
@@ -228,103 +134,16 @@ def run():
                 text='',
                     value=resume_data['skills'],key = '1')
 
-                def callback(param):
-                    print "hello"
+
 
 
                 my_form = st.form(key = "form1")
                 skill = my_form.text_input(label = "What job would you like to search for?")
                 place = my_form.text_input('Where is your job location? ')
-                my_form.form_submit_button(label = "Submit this form", on_click=callback)
+                my_form.form_submit_button(label = "Submit this form") 
                 no_of_pages = int(input('Enter the # of pages to scrape: '))
 
-                indeed_posts=[]
-
-                for page in range(no_of_pages):
-                    url = 'https://www.indeed.com/jobs?q=' + skill + \
-                    '&l=' + place + '&sort=date' +'&start='+ str(page * 10)
-                    response = requests.get(url, headers=headers)
-                    html = response.text
-
-                # Scrapping the Web
-                    soup = BeautifulSoup(html, 'lxml')
-                    
-
-                # Outer Most Entry Point of HTML:
-                    outer_most_point=soup.find('div',attrs={'id': 'mosaic-provider-jobcards'})
-                # "UL" lists where the data are stored:
-                    for i in outer_most_point.find('ul'):
-
-                        # Job Title:
-                        job_title=i.find('h2',{'class':'jobTitle jobTitle-color-purple jobTitle-newJob'})
-                        # print(job_title)
-                        if job_title != None:
-                            jobs=job_title.find('a').text
-
-
-        
-
-
-                        # Company Name:
-                        if i.find('span',{'class':'companyName'}) != None:
-                            company=i.find('span',{'class':'companyName'}).text
-                        # Links:
-                        if i.find('a') != None:
-                            links=i.find('a',{'class':'jcs-JobTitle'})['href']
-                        # Salary if available:
-                        if i.find('div',{'class':'attribute_snippet'}) != None:
-                            salary=i.find('div',{'class':'attribute_snippet'}).text
-                        else:
-                         salary='No Salary'
-
-                    # Job Post Date:
-
-                        if i.find('span', attrs={'class': 'date'}) != None:
-                            post_date = i.find('span', attrs={'class': 'date'}).text
-
-                        # Put everything together in a list of lists for the default dictionary
-                        indeed_posts.append([company,jobs,links,salary, post_date])
-                        print(indeed_posts)
-                    # put together in list
-
-                    # (create a dictionary with keys and a list of values from above "indeed_posts")
-
-            indeed_dict_list=defaultdict(list)
-
-                        # Fields for our DF
-
-            indeed_spec=['Company','job','link','Salary','Job_Posted_Date']
-
-
-            import re # regular expressions
-            import datetime # format date/time
-
-                        # Let's deal with fixing posting dates using Regular Expressions
-
-            indeed_jobs_df = pd.DataFrame(indeed_posts,columns=indeed_spec)
-
-
-            dates_converted=[]
-
-            for i in indeed_jobs_df['Job_Posted_Date']:
-
-                if re.findall(r'[0-9]',i):
-                            # if the string has digits convert each entry to single string: ['3','0']->'30'
-                    b=''.join(re.findall(r'[0-9]',i))
-                            # convert string int to int and subtract from today's date and format
-                    g=(datetime.datetime.today()-datetime.timedelta(int(b))).strftime('%m-%d-%Y')
-
-                    dates_converted.append(g)
-                            # if i.find('today') or i.find('just posted'):
-                            # p=(datetime.datetime.today().strftime('%m-%d-%Y'))
-                            # v.append(p)
-                else: # this will contain strings like: 'just posted' or 'today' etc before convert
-                        dates_converted.append(datetime.datetime.today().strftime('%m-%d-%Y'))
-                dates_converted
-                indeed_jobs_df['Job_Posted_Date']= dates_converted
-
-                df = pd.DataFrame(indeed_posts,columns=indeed_spec)
-                st.dataframe(df)
+                
 
                 #
                 ## Insert into table
